6fa1a1425f493bf59c18dbe7401037d55f502dd6
Gerald LaMountain <gerald.lamountain@outlook.com>
2018-03-15 23:27:54 -0400

    Change test_schur5.m to apply the autocorrelation function from Homework 6 Problem 4

--- test_schur5.m
+++ test_schur5.m
@@ -1,13 +1,20 @@
 clear all
 
-sig = 3;
-alpha = 2;
-cm = @(k)(sig^2 * alpha^abs(k));
+% Demo from class
+%
+% sig = 3;
+% alpha = 2;
+% cm = @(k)(sig^2 * alpha^abs(k));
+% 
+% cmi = [];
+% N = 20;
+% for k = 0:N-1
+%     cmi = [cmi; cm(k)];
+% end
+% 
+% [ Fch, sig_m, k ] = schur5(cmi)
 
-cmi = [];
-N = 20;
-for k = 0:N-1
-    cmi = [cmi; cm(k)];
-end
+% Problem 5
+cm = 0.5*ones(20,1); cm(1) = 1;
 
-[ Fch, sig_m, k ] = schur5(cmi)
+[Fch, sig_m, k] = schur5(cm)
